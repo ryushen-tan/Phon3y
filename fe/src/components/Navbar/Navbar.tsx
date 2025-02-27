25339e5fd35adfc10b0e3e7c198327758b57ecad
ryushen-tan <ryushen@gmail.com>
2025-02-27 03:15:32 -0500

    added a general logo

--- fe/src/components/Navbar/Navbar.tsx
+++ fe/src/components/Navbar/Navbar.tsx
@@ -3,7 +3,12 @@ const Navbar: React.FC = () => {
         <nav className="sticky top-0 z-50 bg-white/80 backdrop-blur-md">
             <div className="absolute flex w-[99.2vw] justify-between z-50">
                 <div className="w-[50%] h-12 py-5 px-8">
-                    <div className="w-[50%] h-12 rounded-full bg-[#DDDDDD] border border-3 border-white" />
+                    <div className="w-[50%] h-12 rounded-full bg-[#DDDDDD] border border-3 border-white flex justify-center items-center cursor-pointer hover:bg-[#FCFCFC] transition-all ease-in-out duration-500">
+                        <div className="w-[96%] h-8 bg-[#FCFCFC] p-3 flex items-center rounded-2xl cursor-pointer flex justify-between">
+                            <h1 className="font-bold font-poppins">Phoney AI</h1>
+                            <hr className="w-80"/>
+                        </div>
+                    </div>
                 </div>
                 <div className="w-[50%] py-5 px-8 flex justify-end">
                     <div className="w-[20%] bg-[#DDDDDD] h-12 rounded-full border border-3 border-white flex pointer-events-auto justify-center items-center cursor-pointer hover:bg-[#FCFCFC] transition-all ease-in-out duration-500">
