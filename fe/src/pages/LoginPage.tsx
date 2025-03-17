9f5d2d86d562374bb33065a7d76a590e02b2ae1f
ryushen-tan <ryushen@gmail.com>
2025-03-17 19:51:25 -0400

    styling changes

--- fe/src/pages/LoginPage.tsx
+++ fe/src/pages/LoginPage.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import GoogleAuth from '../components/GoogleAuth/GoogleAuth';
-
+import Overlay from '/overlay.png';
 const LoginPage: React.FC = () => {
     return (
         <div 
-            className="flex flex-col items-center justify-center h-screen"
-            style={{
-                backgroundImage:
-                    'linear-gradient(-135deg, #8B9CFF 0%, #D5DAF0 50%, #B2C1D2 100%)'
-            }}
+            className="flex bg-white h-screen items-center"
         >
-            <div className='w-120 h-160 rounded-2xl bg-white flex flex-col items-center justify-center'>
-                <h2 className="mb-4 text-xl font-semibold text-gray-500">Login Page</h2>
-                <GoogleAuth />
+            <div className='w-full ml-10'>
+                <img src={Overlay} alt="overlay" />
+
+            </div>
+
+            <div className='rounded-2xl w-[230vw] flex justify-center bg-blue-300'>
+                <div className='flex flex-col justify-center items-center bg-red-300'>
+                    <div className='flex flex-col items-start'>
+                        <h2 className=" text-5xl font-semibold text-gray-500">Phone-In</h2>
+                        <p className='font-semibold text-sm text-gray-500'>Sign in with Partner Account</p>
+                    </div>
+
+                    <GoogleAuth />
+
+                </div>
             </div>
         </div>
     );
